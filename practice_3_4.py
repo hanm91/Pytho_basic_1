ac5d81da594a75b8120e712f8a4b15c84be8bcd6
mikhail <bamnsk@gmail.com>
2020-04-21 17:55:22 +0700

    complite practice_3__

--- practice_3_4.py
+++ practice_3_4.py
@@ -1,5 +1,24 @@
-def my_func(x, y):
+def my_func(x: int, y: int) -> int:
+    """
+    Принимает два аргумента х и у и возвращает х в степени у.
+
+    """
     c = x**y
     return c
 
 print (my_func(2, 6))
+
+def my_func2 (x:int, y:int) -> int:
+    """
+    Принимает два аргумента х и у и возвращает х в степени у.
+
+    """
+    res = 1
+    for i in range(abs(y)):
+        res *= x
+    if y >= 0:
+        return res
+    else:
+        return 1 / res
+
+print (my_func2(2, 2))
